94dbffac3f062b2b76d0f981533e14b030144d6e
sarahg813 <goon.sarah@gmail.com>
2020-10-12 20:13:13 -0400

    updated package.json

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
-require("./stylesheets/main.scss");
 
 ReactDOM.render(
   <React.StrictMode>
